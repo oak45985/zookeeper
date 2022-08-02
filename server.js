432ed14cba97743f967d26194bbff349b6626c3f
oak45985 <oak45985@gmail.com>
2022-08-02 06:53:39 -0500

    404 error set up

--- server.js
+++ server.js
@@ -44,6 +44,20 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 } 
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
+app.get('/api/animals/:id',(req,res) => {
+    const result = findById(req.params.id,animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    };
+});
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
